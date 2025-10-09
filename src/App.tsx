524ef96c045a03dc9da62bb8fe474e6e6dc3f7c7
MetaSearch-IO <weiyuzhi2022@gmail.com>
2025-10-09 13:29:33 +0800

    feat: set up Trewth Interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import TokenLaunch from "./components/TokenLaunch";
 
 function App() {
     return (
         <div className="App">
+            <TokenLaunch />
         </div>
     );
 }
