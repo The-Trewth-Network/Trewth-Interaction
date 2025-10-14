b49373ee4cc2b450c8a68dc305a7ebb13fd3790a
MetaSearch-IO <weiyuzhi2022@gmail.com>
2025-10-14 15:22:07 +0800

    feat: set up heatmap compts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TokenLaunch from "./components/TokenLaunch";
 import Trading from "./components/Trading";
+import EventCoinMonitor from "./components/EventCoinMonitor";
 
 function App() {
     return (
         <div className="App">
             <TokenLaunch />
             <Trading />
+            <EventCoinMonitor />
         </div>
     );
 }
--- src/components/EventCoinMonitor.tsx
+++ src/components/EventCoinMonitor.tsx
@@ -1,1 +1,14 @@
-export {}
+import React from "react";
+import CoinLiquity from "./EventCoinMonitor/CoinLiquity";
+
+const EventCoinMonitor: React.FC = () => {
+    return (
+        <div>
+            <h2>事件币池监控</h2>
+            <CoinLiquity />
+        </div>
+    );
+};
+
+export default EventCoinMonitor;
+
